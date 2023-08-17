2a7f12b063a166c15369d3cb1a71742a42034053
OleksandrBabilia <djesssash1708@gmail.com>
2023-08-17 20:33:05 +0300

    Added: Zip files support

--- modules/logs/routers.py
+++ modules/logs/routers.py
@@ -1,6 +1,9 @@
+import zipfile
+import io
+
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
@@ -19,14 +22,35 @@ def upload_logs(
     file: UploadFile = File(...),
     db: Session = Depends(get_db),
 ):
-    file_path = save_uploaded_file(file)
+    if file.content_type != "application/zip":
+        file_path = save_uploaded_file(file)
     # Assuming logs are stored as plain text lines in the file
-    with open(file_path, "r") as f:
-        for line in f:
-            log_entry = LogEntry(message=line.strip())
-            db.add(log_entry)
+        with open(file_path, "r") as f:
+            for line in f:
+                log_entry = LogEntry(message=line.strip())
+                db.add(log_entry)
+            db.commit()
+        return {"message": "Logs uploaded successfully"}
+
+    # Extract files from the uploaded ZIP archive
+    zip_file_content = io.BytesIO(file.file.read())
+
+    # Extract files from the uploaded ZIP archive
+    with zipfile.ZipFile(zip_file_content, "r") as zip_ref:
+        extracted_files = []
+        for zip_info in zip_ref.infolist():
+            if not zip_info.filename.endswith('/') and (zip_info.filename.endswith('.log') or zip_info.filename.endswith('.txt')):
+                extracted_files.append(zip_info.filename)
+                log_data = zip_ref.read(zip_info.filename).decode("utf-8")
+                with io.StringIO(log_data) as log_stream:
+                    for line in log_stream:
+                        log_entry = LogEntry(message=line.strip())
+                        db.add(log_entry)
         db.commit()
-    return {"message": "Logs uploaded successfully"}
+
+    return {"message": f"Logs uploaded from {len(extracted_files)} log files in the ZIP archive"}
+
+
 
 
 @logs_router.get("/")
